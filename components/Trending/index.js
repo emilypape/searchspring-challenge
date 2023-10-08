935fdc99d89dd042e7fd286a4d6f8d46006ecde8
emily <eap6787@gmail.com>
2023-10-08 15:08:10 -0600

    add shopping icon to trending page

--- components/Trending/index.js
+++ components/Trending/index.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { Icon } from '@iconify/react';
 import Image from 'next/image';
 export default function Trending() {
   const [trending, setTrending] = useState();
@@ -25,11 +26,17 @@ export default function Trending() {
         {trending?.results?.map((item) => {
           return (
             <div className='flex' key={item.uid}>
-              <div className='p-3 min-h-[30em]'>
-                <Image alt='item photo' src={item.imageUrl} width={250} height={250} />
+              <div className='p-3 min-h-[32em]'>
+                <div className=''>
+                  <Image alt='item photo' src={item.imageUrl} width={250} height={250} />
+                  <div className='flex justify-end mt-[-2em] mb-4 mr-4 hover:text-zinc-500 cursor-pointer'>
+                    <Icon icon='bi:bag-plus' width={25} height={25} />
+                  </div>
+                </div>
+
                 <div className='max-w-[15em] text-md'>{item.name}</div>
                 <div className='text-gray-400 text-sm'>{item.keywords[2]}</div>
-                <div className='flex'>
+                <div className='flex '>
                   {item.price < item.msrp ? (
                     <div className='text-gray-400 line-through'>${item.msrp}</div>
                   ) : (
