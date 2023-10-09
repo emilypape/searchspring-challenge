4c83b2e7beb7ddf5c6a45143e9b3a29f4f91b91d
emily <eap6787@gmail.com>
2023-10-09 15:46:51 -0600

    nuclear cleanup isle trending

---
+++ pages/trending/index.js
@@ -0,0 +1,11 @@
+import Trending from '../../components/Trending';
+import Nav from '../../components/Nav';
+
+export default function TrendingPage() {
+  return (
+    <div>
+      <Nav />
+      <Trending />
+    </div>
+  );
+}
